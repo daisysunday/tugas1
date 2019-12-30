799e0dc0c26c932081e2cf52d1d514b40874fbd9
daisysunday <abbymanyu28@gmail.com>
2019-12-30 16:43:17 +0700

    tugas1 bahasa setting

--- app/src/main/java/com/example/profil/MainActivity.java
+++ app/src/main/java/com/example/profil/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.profil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView ivSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivSetting = findViewById(R.id.ivSetting);
+        ivSetting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_follow:
+                onStop();
+                break;
+            case R.id.ivSetting:
+                Intent setting = new Intent(MainActivity.this, AppSetting.class);
+                startActivity(setting);
+                return;
+        }
     }
 }
+
